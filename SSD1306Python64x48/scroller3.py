ad998ab54220bd5bd934456cf21e3e0bc96c8c63
Tobias van Dyk <TobiasJVanDyk@gmail.com>
2020-09-17 21:17:55 +0200

    Update scroller3.py

--- SSD1306Python64x48/scroller3.py
+++ SSD1306Python64x48/scroller3.py
@@ -5,13 +5,13 @@
 #
 # Copyright (c) 2017 Adafruit Industries
 # Author: Tony DiCola & James DeVito
-#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
-# furnished to do so.
+# furnished to do so. This copyright notice and this permission notice shall be 
+# included in all copies or substantial portions of the Software.
 #######################################################################################
 
 import time
